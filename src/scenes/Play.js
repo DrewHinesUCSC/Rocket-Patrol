15645c6bdc50603fe3673dcf9db70e96169ad3aa
DrewHinesUCSC <drhines@ucsc.edu>
2026-01-14 22:47:16 -0800

    Game Completed(1)

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -59,15 +59,36 @@ class Play extends Phaser.Scene {
             fixedWidth: 100
         }
         this.scoreLeft = this.add.text(borderUISize + borderPadding, borderUISize + borderPadding * 2, this.p1Score, scoreConfig)
+
+        //GAME OVER flag
+        this.gameOver = false
+
+        //60 second play clock
+        scoreConfig.fixedWidth = 0
+        this.clock = this.time.delayedCall(game.settings.gameTimer, () => {
+            this.add.text(game.config.width/2, game.config.height/2, 'GAME OVER', scoreConfig).setOrigin(0.5)
+            this.add.text(game.config.width/2, game.config.height/2 + 64, 'Press (R) to Restart or <- for Menu', scoreConfig).setOrigin(0.5)
+            this.gameOver = true
+        }, null, this)
     }
 
     update(){
-        this.starfield.tilePositionX -= 4
-        this.p1Rocket.update()
 
+        //check key input for restart
+        if(this.gameOver && Phaser.Input.Keyboard.JustDown(keyRESET)){
+            this.scene.restart()
+        }
+        if(this.gameOver && Phaser.Input.Keyboard.JustDown(keyLEFT)){
+            this.scene.start('menuScene')
+        }
+
+        this.starfield.tilePositionX -= 4
+        if(!this.gameOver){
+        this.p1Rocket.update()
         this.ship01.update()
         this.ship02.update()
         this.ship03.update()
+        }
 
         //check collisions
         if(this.checkCollision(this.p1Rocket, this.ship03)){
@@ -110,5 +131,7 @@ class Play extends Phaser.Scene {
         //score add and text update
         this.p1Score += ship.points
         this.scoreLeft.text = this.p1Score
+
+        this.sound.play('sfx-explosion')
     }
 }
